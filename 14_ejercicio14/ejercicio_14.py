c751a52b0942e7042d47f2740685aed29a9686cc
Lionel Román Belén <lionelromanbelen7@gmail.com>
2025-08-21 04:25:35 -0300

    ejercicios agregados al repositorio

--- 14_ejercicio14/ejercicio_14.py
+++ 14_ejercicio14/ejercicio_14.py
@@ -1,6 +1,23 @@
 from PIL import Image
 import numpy as np
 
+#metodo para invertir la matriz
+def invertir_columnas(matriz):
+    filas = len(matriz)  
+    columnas = len(matriz[0])
+    matriz_invertida = []
+
+    #Inicialización de matriz vacía con ceros
+    for i in range(filas):
+        fila = [0] * columnas       
+        matriz_invertida.append(fila)
+
+    #Inversión horizontal: copiar columnas en orden inverso
+    for i in range(filas):
+        for j in range(columnas):
+            matriz_invertida[i][j] = matriz[i][columnas - 1 - j]
+    return matriz_invertida
+
 #1 Cargamos la imagen
 imagen = Image.open('imagen/gojo.jpg')
 
@@ -10,11 +27,12 @@ imagen_gris = imagen.convert('L')
 #3 Convertir a matriz NumPy
 matriz = np.array(imagen_gris)
 
+print(matriz)
 #4 Voltear horizontalmente (invertir columnas)
-matriz_volteada = np.fliplr(matriz)
-
+matriz_invertida = invertir_columnas(matriz)
+        
 #5 Convertir la matriz volteada a imagen
-imagen_volteada = Image.fromarray(matriz_volteada)
+imagen_volteada = Image.fromarray(np.array(matriz_invertida))
 
 #6 Mostrar imagen volteada
 imagen_volteada.show()
